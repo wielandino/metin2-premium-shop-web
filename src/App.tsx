609e3b65dee6d5733f2a87a2f369e09f0c90e9b7
wielandino <wieland-f.wueller@outlook.de>
2026-01-29 23:29:44 +0100

    started with ShopPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ItemDescriptionPage } from "./pages/ItemDescriptionPage"
+import ShopPage from "./pages/ShopPage"
 import { SHOP_ITEM_DUMMY_MODEL_1, SHOP_ITEM_DUMMY_MODEL_2 } from "./testing/ShopItemMocking"
 
 function App() {
 
   return (
     <>
-      <ItemDescriptionPage shopItem={SHOP_ITEM_DUMMY_MODEL_1} />
+      <ShopPage />
     </>
   )
 }
--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -2,9 +2,9 @@ function ShopPage() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        ShopPage
-      </h1>
+      <div className="metin-container page-image mx-auto">
+        <div className="bg-transparent sshadow-[0_2px_6px_#662d12 h-10 text-[#f2e69f] pl-5 pr-12 py-1.5"></div>
+      </div>
     </>
   )
 }
